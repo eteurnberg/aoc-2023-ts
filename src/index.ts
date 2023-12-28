20981e6f132a324dbd95bbe6a663bccacf6adfd5
Emil Teurnberg <e.teurnberg@gmail.com>
2023-12-28 17:57:58 +0100

    chore: add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-console.log('hello');
+// src/index.js
+const favoriteFruits = ["apple", "strawberry", "orange"];
+
+function addFruit(fruit: string) {
+    favoriteFruits.push(fruit);
+}
+
+addFruit('banana');
+
+console.log(`fruits: ${favoriteFruits}`);
 
