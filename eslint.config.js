51d9b26c36b4041d455bcd0d51210253b2322f8e
Emil Teurnberg <e.teurnberg@gmail.com>
2023-12-30 16:51:31 +0100

    chore: improve eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,14 @@ import typescriptEslintParser from '@typescript-eslint/parser';
 import eslintJs from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
   eslintJs.configs['recommended'],
   eslintConfigPrettier,
+  {
+    // global ignores
+    ignores: ['build/**', 'node_modules/**'],
+  },
   {
     files: ['**/*.ts'],
     languageOptions: {
